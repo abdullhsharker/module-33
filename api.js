c4b2b2b3ad3fc4219b4d2f4918b855bf54fc74d9
abdullhsharker <abdullahsharker99@gmail.com>
2022-03-04 02:19:45 +0600

    add 33.5

--- api.js
+++ api.js
@@ -3,3 +3,33 @@ function loadData() {
         .then(response => response.json())
         .then(json => console.log(json))
 }
+//========================================
+// json placeholder theke 10 user ke nilam
+//========================================
+
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+
+        .then(data => displayUsers(data));
+}
+
+
+//========================================
+// json placeholder theke 100 post nilam
+//========================================
+function loadPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+function displayUsers(data) {
+    const ul = document.getElementById('users');
+    for (const user of data) {
+        console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
+}
